edbebb9e96dbea2fcc0d33953122b2e1e175e0bf
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-07 19:51:02 +0800

    Delete test directory

--- test/each_test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert'),å
-    csv = require("../index");
-
-var count = 0;
-csv.each(__dirname + '/basic_sample.csv').on("data", function(data) {
-  count += 1;
-}).on("complete", function() {
-  assert.equal(count, 1800);
-});
